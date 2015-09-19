69c272c0d640bbc382291082cce348dae56907c0
JAMES BARNETT <jbarnett@JAMESs-MacBook-Pro.local>
2015-09-18 22:21:03 -0400

    drawing working, with colors.

--- js/test2.js
+++ js/test2.js
@@ -1,13 +1,12 @@
-var started = false;
-var paint = false;
-var prevX = 0;
-var currX = 0;
-var prevY = 0;
-var currY = 0;
-var flag = false;
-var dot_flot = false;
-var canvas = null;
-var ctx = null;
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+var x = "black",
+    y = 2;
 
 $( document ).ready(function() {
   
@@ -40,36 +39,26 @@ $( document ).ready(function() {
     var drawingType = $(this).attr('id');
     console.log(drawingType);
   });
-
-  canvas = document.getElementById('myMainCanvas');
-  ctx = canvas.getContext("2d");
-  w = canvas.width;
-  h = canvas.height;
-
-
-  document.getElementById("myMainCanvas").addEventListener("mousemove", function (e) {
-        findxy('move', e)
-    }, false);
-  document.getElementById("myMainCanvas").addEventListener("mousedown", function (e) {
-        findxy('down', e)
-    }, false);
-  document.getElementById("myMainCanvas").addEventListener("mouseup", function (e) {
-        findxy('up', e)
-    }, false);
-  document.getElementById("myMainCanvas").addEventListener("mouseout", function (e) {
-        findxy('out', e)
-    }, false);
-
-
-
 });
 
-function ev_paintstart() {
-  paint = true;
-}
+function init() {
+    canvas = document.getElementById('can');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
 
-function ev_paintstop() {
-  paint = false;
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
 }
 
 function color(obj) {
@@ -99,6 +88,31 @@ function color(obj) {
     if (x == "white") y = 14;
     else y = 2;
 
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function erase() {
+    var m = confirm("Want to clear");
+    if (m) {
+        ctx.clearRect(0, 0, w, h);
+        document.getElementById("canvasimg").style.display = "none";
+    }
+}
+
+function save() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
 }
 
 function findxy(res, e) {
@@ -131,16 +145,3 @@ function findxy(res, e) {
         }
     }
 }
-
-function draw() {
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(currX, currY);
-    ctx.strokeStyle = x;
-    ctx.lineWidth = y;
-    ctx.stroke();
-    ctx.closePath();
-}
-
-
-
